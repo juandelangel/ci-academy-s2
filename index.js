e42517687b2ed8eececed3bcce6c3a0885bfbb60
JuanDelAngel <juan.delangel.condor@gmail.com>
2021-04-16 08:46:00 -0500

    added route

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const https = require('https');
 const fs = require('fs');
+
+app.get('/home', (req, res) => {
+    res.sen('<h1>Hello2!</h1>')
+})
+
 const certPath = '/etc/letsencrypt/live/condorinnovationacademy.online';
 https.createServer({
     key: fs.readFileSync('/etc/letsencrypt/live/condorinnovationacademy.online/privkey.pem'),
