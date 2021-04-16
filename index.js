ec4ea7b3ce5dd81e5b48bb6bed64887c0d73751a
JuanDelAngel <juan.delangel.condor@gmail.com>
2021-04-16 00:33:17 -0500

    updated paths

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const https = require('https');
 const fs = require('fs');
 const certPath = '/etc/letsencrypt/live/condorinnovationacademy.online';
 https.createServer({
-    key: fs.readFileSync('${certPath}/privkey.pem'),
-    cert: fs.readFileSync( '${certPath}/fullchain.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/condorinnovationacademy.online/privkey.pem'),
+    cert: fs.readFileSync( '/etc/letsencrypt/live/condorinnovationacademy.online/fullchain.pem')
 }, (req, res) => { res.end('<h1>Hello!</h1>') }
 ).listen(443);
