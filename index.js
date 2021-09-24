051568d1a7cbb45e29fc5b824c4159c118c8f7ad
juan <juan.delangel.condor@gmail.com>
2021-09-24 10:51:45 -0500

    Expire value of the token updated to 1500 seg

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/auth', (req, res) => {
    check:  true
   };
   const token = jwt.sign(payload, app.get('key'), {
-   expiresIn: 60
+   expiresIn: 1500
   });
   res.json({
    status:200,
